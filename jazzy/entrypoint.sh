3369d13bc0bd1a649e0fa3ec8bc92139a50eb2d2
Daisuke Sato <tiryoh@gmail.com>
2024-12-28 17:52:18 +0900

    feat: Enable Gazebo on arm64 Jazzy/Rolling

--- jazzy/entrypoint.sh
+++ jazzy/entrypoint.sh
@@ -349,13 +349,17 @@ Icon=vscodium
 EOF
 chown -R "$USER:$USER" "$HOME/Desktop"
 
+echo "============================================================================================"
+echo "Launched docker container."
+echo -e 'Open \e]8;;http://127.0.0.1:6080\e\\http://127.0.0.1:6080\e]8;;\e\\ via web browser.'
+echo ""
+echo "NOTE 1: Default user is \"$USER\", password is \"$PASSWORD\"."
+echo "NOTE 2: --security-opt seccomp=unconfined flag is required to launch Ubuntu Jammy/Noble based image on some environment."
+echo -e 'See \e]8;;https://github.com/Tiryoh/docker-ros2-desktop-vnc/pull/56\e\\https://github.com/Tiryoh/docker-ros2-desktop-vnc/pull/56\e]8;;\e\\'
+echo "============================================================================================"
+
 # clearup
 PASSWORD=
 VNC_PASSWORD=
-
-echo "============================================================================================"
-echo "NOTE 1: --security-opt seccomp=unconfined flag is required to launch Ubuntu Jammy based image."
-echo -e 'See \e]8;;https://github.com/Tiryoh/docker-ros2-desktop-vnc/pull/56\e\\https://github.com/Tiryoh/docker-ros2-desktop-vnc/pull/56\e]8;;\e\\'
-echo "============================================================================================"
 
 exec /bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
